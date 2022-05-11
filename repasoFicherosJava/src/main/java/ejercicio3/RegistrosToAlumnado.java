9df005cc894ad7f0e215a608f1d8937d71ac484d
Nacho <jsalcedoguarde@gmail.com>
2022-05-11 10:58:31 +0200

    añadido metodo conversion de listas a la clase registroToAlumnado

--- repasoFicherosJava/src/main/java/ejercicio3/RegistrosToAlumnado.java
+++ repasoFicherosJava/src/main/java/ejercicio3/RegistrosToAlumnado.java
@@ -5,11 +5,40 @@
  */
 package ejercicio3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeMap;
+
 /**
  *
  * @author Windows10
  */
 public class RegistrosToAlumnado {
     
+    public static  List<Alumnado> conversionListas (List<RegistroJSON> auxEntrada){
+        List<Alumnado> auxSalida = new ArrayList<>();
+          for (RegistroJSON registro : auxEntrada) {
+            Alumnado alumno = new Alumnado();
+            alumno.setNombre(registro.getAlumnoA());
+            TreeMap<String,String> notas = new TreeMap<>();
+
+            notas.put("CEAC",registro.getcEAC());
+            notas.put("EA", registro.geteA());
+            notas.put("TC", registro.gettC());
+            notas.put("FOL", registro.getfOL());
+            notas.put("ING", registro.getiNG());
+            notas.put("TII",registro.gettII());
+            alumno.setNotas(notas);
+
+            auxSalida.add(alumno);
+        }
+          Collections.sort(auxSalida);
+
+        return auxSalida;
     
+}
+            
+            
+            
 }
